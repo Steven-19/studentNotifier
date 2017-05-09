2442538061c02d72368973b70c3fa1757b464a82
Steven Mai <stevenmai132@yahoo.ca>
2017-05-08 21:06:13 -0400

    dkfhakdjhfad

--- facebook/verification.js
+++ facebook/verification.js
@@ -1,4 +1,4 @@
-var crypto = require('crypto');
+const crypto = require('crypto');
 module.exports.verifyRequestSignature = function(req, res, buf) {
   var signature = req.headers["x-hub-signature"];
 
